d8351d519aa25db38944edf63471c4aad07dfedf
TobiasGI <tobiasgarciaiacovelli@gmail.com>
2025-06-25 17:19:40 -0300

    Cambios graficos

--- trabajo_integrador/init_db.py
+++ trabajo_integrador/init_db.py
@@ -20,7 +20,8 @@ CREATE TABLE brainrot (
     resistencia INTEGER,
     inteligencia INTEGER,
     carisma INTEGER,
-    aura INTEGER
+    aura INTEGER,
+    descripcion TEXT
 )
 """)
 
@@ -35,17 +36,20 @@ CREATE TABLE users (
 
 # Datos
 c.executemany(
-    "INSERT INTO brainrot (nombre, imagen, fuerza, velocidad, resistencia, inteligencia, carisma, aura) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+    "INSERT INTO brainrot (nombre, imagen, fuerza, velocidad, resistencia, inteligencia, carisma, aura, descripcion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
     [
-        ("Tung tung tung Sahur", "Tung_tung_tung_sahur.png", 90, 30, 70, 10, 30, 100),
-        ("Tralalero Tralala", "Traralelo_tralala.png", 70, 70, 40, 50, 10, 85),
-        ("Ballerina Capuccina", "ballerina-capuccina.png", 40, 100, 480, 80, 60, 50),
-        ("Bombardino Crocodilo", "Bombardino_crocodilo.png", 100, 30, 50, 30, 10, 90),
-        ("Brr Brr Patapim", "Brr_brr_patapim.png", 60, 60, 60, 100, 80, 70),
-        ("Lirila Larila", "Lirili_larila.png", 90, 10, 90, 80, 70, 40),
-        ("Desarrollinni Segurinni", "Desarrollinni_segurinni.png", 75, 75, 75, 75, 50, 0),
+        ("Tung tung tung Sahur", "Tung_tung_tung_sahur.png", 90, 30, 70, 10, 30, 100,
+         """Tung tung tung tung tung tung tung tung tung l'alba
+Un'anomalia terrificante che appare solo all'alba"""),
+        ("Tralalero Tralala", "Traralelo_tralala.png", 70, 70, 40, 50, 10, 85, "Tralalero Tralala, porco Dio e porco Allah. Ero con il mio fottuto figlio Merdardo a giocare a Fortnite, quando a un punto arriva mia nonna"),
+        ("Ballerina Capuccina", "ballerina-capuccina.png", 40, 100, 480, 80, 60, 50, "Ballerina Capuchina mi mi mi. È la moglie di Cappuccino Assassino"),
+        ("Bombardino Crocodilo", "Bombardino_crocodilo.png", 100, 30, 50, 30, 10, 90, "Bombardiro Crocodilo, un fottuto alligatore volante, che vola e bombarda.Si nutre dello spirito di tua madre."),
+        ("Brr Brr Patapim", "Brr_brr_patapim.png", 60, 60, 60, 100, 80, 70, "Brr, brr, Patapim, il mio cappello è pieno di Slim! Nel bosco fitto e misterioso, viveva un essere assai curioso."),
+        ("Lirila Larila", "Lirili_larila.png", 90, 10, 90, 80, 70, 40, "Lirilí Larilà, elefante nel deserto che cammina qua e là. Con la sua conchiglia e un orologio che fa tic tac, le spine del cactus mi fanno"),
+        ("Desarrollinni Segurinni", "Desarrollinni_segurinni.png", 75, 75, 75, 75, 50, 0, "Flag"),
     ]
 )
+
 
 
 c.executemany("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", [
